fdf9012b3525b98f532681e8a72975059b852721
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-09-26 13:42:27 -0700

    lab11

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('oh hello there');
+
+/// GLOBAL VARIABLES
+let voteTotal = 25;
+let productArray = [];
+
+// DOM RENDERING
+let imgContainer = document.getElementById('img-container');
+let imgOne = document.getElementById('img-one');
+let imgTwo = document.getElementById('img-two');
+
+// CONSTRUCTOR FUNCTION
+
+
+// HELPER FUNCTION / UTILITIES
+
+
+// EVENT HANDLERS
+
+//EXCUTABLE CODE
